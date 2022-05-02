1b45146bb3940f51da9dec1421da998698e3296a
Onella <nellywebdev@gmail.com>
2022-05-02 19:15:25 -0400

    Add and modify About Me section in my index.html Add to my js.index a submit button, remove and edit button

--- js/index.js
+++ js/index.js
@@ -1,4 +1,4 @@
-const today= new Date()
+const today= new Date() /* footer name and year*/ 
 const thisYear= today.getFullYear()
 const footer= document.querySelector("footer")
 const copyright= document.createElement("p")
@@ -7,7 +7,7 @@ footer.appendChild(copyright)
 
 console.log(footer)
 
-const skills = ["HTML", "CSS", "JavaScript","Visual Studios","Travel Consultant"]
+const skills = ["HTML", "CSS", "JavaScript","Visual Studios","Travel Consultant"] /* skills */
 const skillsSection = document.querySelector("#skills")
 
 const skillsList = skillsSection.querySelector("ul")
@@ -17,4 +17,59 @@ const skillsList = skillsSection.querySelector("ul")
        skillsList.appendChild(skill)
     }
  
-    
+    const messageForm= document.getElementsByTagName("form")[0]; /* form */
+    console.log(messageForm);
+
+messageForm.addEventListener("submit", (event) => { /* submitting form */
+    event.preventDefault();
+  const name = event.target.name.value;
+  const email= event.target.email.value;
+  const message = event.target.message.value;
+
+  console.log(event.target.name.value,event.target.email.value,event.target.message.value);
+
+  const messageSection= document.querySelector("#messages"); /* list of message dispaly */
+  const messageList= messageSection.querySelector("ul");
+  const newMessage= document.createElement("li"); 
+
+  console.log(email);
+  console.log(name);
+
+  newMessage.innerHTML= `<a href = mailto:${email}>${name}</a>wrote:<span>${message}</span>` /*message dispaly */
+
+  const removeButton= document.createElement("button");  /* remove button */
+  removeButton.innerText= "remove";
+  removeButton.addEventListener("click", (event) => {
+      const entry= event.target.parentNode;
+      entry.remove();
+  });
+
+  const editButton= document.createElement("button"); /* edit button */
+  editButton.textContent= "edit"; 
+  editButton.addEventListener("click", (event) => {
+    const entry= event.target.parentNode;
+    const span = entry.querySelector("span");
+        if (editButton.textContent === "edit") {
+    console.log(entry);
+    const editText = document.createElement("input"); /* message input to edit */
+    console.log(editText);
+        editText.type="text";
+        editText.value = span.textContent;
+        editButton.textContent= "save"; 
+        entry.appendChild(editText);
+        editButton.textContent = "save"; 
+        } else if (editButton.textContent === "save"){ /* save edit */
+            const input = entry.querySelector("input");
+            console.log(input);
+            span.textContent= input.value;
+            entry.removeChild(input);
+            editButton.textContent= "edit";
+    }    
+  });
+  newMessage.appendChild(editButton);
+  newMessage.appendChild(removeButton);
+  messageList.appendChild(newMessage);
+  messageForm.reset();
+});
+
+
